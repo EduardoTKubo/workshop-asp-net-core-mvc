dead9295ad17cdcf1f2f50a64095368d93a20b59
ekubo <eduardo.tkb@outlook.com>
2018-12-05 19:04:22 -0300

    crud scaffolding

---
+++ Aula146_SalesWebMvc/Data/Aula146_SalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula146_SalesWebMvc.Models
+{
+    public class Aula146_SalesWebMvcContext : DbContext
+    {
+        public Aula146_SalesWebMvcContext (DbContextOptions<Aula146_SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula146_SalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
